fe30c144fff12af4293fe0bd17277a383b97777d
unknown <micjanic20@gmail.com>
2021-05-24 23:35:14 -0500

    setup post

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { useQuery } from "react-query";
 import "./App.css";
 
+import Button from "@material-ui/core/Button";
+
 const fetchPins = async () => {
   const response = await fetch("http://192.168.1.225/gpio_api/pins/");
   const data = await response.json();
   return data;
 };
 
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "PATCH",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
 function App() {
   const { data, isLoading, error } = useQuery("getAll", fetchPins);
   console.log(data, isLoading, error);
 
-  return isLoading ? <></> : <div className="App">{data[0].pin_num}</div>;
+  return isLoading ? (
+    <></>
+  ) : (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 }
 
 export default App;
